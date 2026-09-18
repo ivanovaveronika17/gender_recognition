064c56afd3780c9d3e5faef9d7483f1e31f562c0
ivanovaveronika17 <ivanovaveronika1705@gmail.com>
2026-09-18 10:07:37 +0200

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -11,9 +11,6 @@ from sklearn.metrics import (
 
 from pathlib import Path
 
-
-# 1. Поставки
-
 IMAGE_SIZE = (224, 224)
 BATCH_SIZE = 32
 
@@ -22,15 +19,9 @@ project_folder = Path(__file__).parent
 test_folder = project_folder / "dataset" / "test"
 model_path = project_folder / "models" / "gender_model.keras"
 
-
-# 2. Го вчитуваме моделот
-
 model = tf.keras.models.load_model(model_path)
 
 print("Моделот е успешно вчитан!")
-
-
-# 3. Ги вчитуваме test сликите
 
 test_dataset = tf.keras.utils.image_dataset_from_directory(
     test_folder,
@@ -42,17 +33,11 @@ test_dataset = tf.keras.utils.image_dataset_from_directory(
     shuffle=False
 )
 
-
-# 4. Нормализација
-
 normalization_layer = tf.keras.layers.Rescaling(1.0 / 255)
 
 test_dataset = test_dataset.map(
     lambda x, y: (normalization_layer(x), y)
 )
-
-
-# 5. Ги земаме вистинските labels
 
 true_labels = []
 
@@ -61,15 +46,9 @@ for images, labels in test_dataset:
 
 true_labels = np.array(true_labels).astype(int)
 
-
-# 6. Моделот прави предвидувања
-
 predictions = model.predict(test_dataset)
 
 predicted_labels = (predictions >= 0.5).astype(int).flatten()
-
-
-# 7. Ги пресметуваме метриките
 
 accuracy = accuracy_score(
     true_labels,
@@ -86,18 +65,12 @@ recall = recall_score(
     predicted_labels
 )
 
-
-# 8. Ги прикажуваме резултатите
-
 print()
 print("===== RESULTS =====")
 
 print(f"Accuracy:  {accuracy:.4f}")
 print(f"Precision: {precision:.4f}")
 print(f"Recall:    {recall:.4f}")
-
-
-# 9. Confusion Matrix
 
 cm = confusion_matrix(
     true_labels,
@@ -107,9 +80,6 @@ cm = confusion_matrix(
 print()
 print("Confusion Matrix:")
 print(cm)
-
-
-# 10. Classification Report
 
 print()
 print("Classification Report:")
