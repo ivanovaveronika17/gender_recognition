d4f8e83ad9f4060a4b97fc1e01e5522196df6564
ivanovaveronika17 <ivanovaveronika1705@gmail.com>
2026-09-18 10:08:59 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -2,10 +2,10 @@ import cv2
 
 
 def preprocess_face(face):
-    # Ја менуваме големината на лицето на 224 x 224
+   
     face = cv2.resize(face, (224, 224))
 
-    # Ги претвораме вредностите на пикселите од 0-255 во 0-1
+  
     face = face / 255.0
 
     return face
